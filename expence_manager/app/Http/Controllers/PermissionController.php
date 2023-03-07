fac55628d18ccaf3913429f9b062c578c620f761
hitesh <hiteshr@zignuts.com>
2023-03-07 10:00:12 +0530

    PermissionController modified

--- expence_manager/app/Http/Controllers/PermissionController.php
+++ expence_manager/app/Http/Controllers/PermissionController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Permission;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PermissionController extends Controller
 {
